d4cd883af6a27c65b148342ac4109230e95c5fd2
harunwebhost <harunwebhost@gmail.com>
2016-07-21 12:29:27 +0530

    Template divided

---
+++ template_module/footer.php
@@ -0,0 +1,20 @@
+ <!-- jQuery -->
+    <script src="required_javascript/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="required_javascript/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="required_javascript/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="required_javascript/raphael-min.js"></script>
+    <script src="required_javascript/morris.min.js"></script>
+    <script src="required_javascript/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="required_javascript/sb-admin-2.js"></script>
+
+</body>
+
+</html>
